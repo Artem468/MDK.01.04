96ae290003a1ceea5573158b6c493c81aff6fb21
Artem468 <56316884+Artem468@users.noreply.github.com>
2024-09-19 10:34:38 +0200

    Add files via upload

---
+++ uspel_ne_uspel/Program.cs
@@ -0,0 +1,9 @@
+﻿// Я не знаю зачем тут потоки или процессы все работает и без них :)
+
+Console.WriteLine("Через 3 секунды нажмите на кнопку");
+Thread.Sleep(3000);
+DateTime startTime = DateTime.Now;
+Console.WriteLine("Сейчас!");
+Console.ReadKey();
+TimeSpan reactionData = DateTime.Now - startTime;
+Console.WriteLine($"Вам потребовалось: {reactionData.TotalMilliseconds:F0} миллиссекунда");
