cf47038d4e9c98dc13f19afc30fa9ffbd77ac1c0
Artem468 <56316884+Artem468@users.noreply.github.com>
2024-09-21 12:53:51 +0200

    Update Program.cs
    теперь с потоком :)

--- uspel_ne_uspel/Program.cs
+++ uspel_ne_uspel/Program.cs
@@ -1,9 +1,19 @@
-﻿// Я не знаю зачем тут потоки или процессы все работает и без них :)
+Console.WriteLine("Нажмите Enter, чтобы начать");
+Console.ReadKey(true);
+Console.WriteLine("Скоро появится сигнал");
 
-Console.WriteLine("Через 3 секунды нажмите на кнопку");
-Thread.Sleep(3000);
-DateTime startTime = DateTime.Now;
-Console.WriteLine("Сейчас!");
-Console.ReadKey();
-TimeSpan reactionData = DateTime.Now - startTime;
-Console.WriteLine($"Вам потребовалось: {reactionData.TotalMilliseconds:F0} миллиссекунда");
+Thread signalThread = new Thread(SignalUser);
+signalThread.Start();
+
+
+void SignalUser()
+{
+    Random random = new Random();
+    int delay = random.Next(1000, 5000);
+    Thread.Sleep(delay);
+    Console.WriteLine("Нажмите любую клавишу!");
+    DateTime startTime = DateTime.Now;
+    Console.ReadKey(true);
+    TimeSpan reactionData = DateTime.Now - startTime;
+    Console.WriteLine($"Ваше время реакции: {reactionData.TotalMilliseconds:F0} миллисекунд");
+}
